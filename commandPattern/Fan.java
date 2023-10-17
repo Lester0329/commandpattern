a4f226f93a9a525c643cc0a7fa964eb2ce3c1bf3
Lester0329 <72334101+Lester0329@users.noreply.github.com>
2023-10-17 23:58:02 +0800

    Create Fan.java

---
+++ commandPattern/Fan.java
@@ -0,0 +1,21 @@
+package commandPattern;
+
+public class Fan {
+
+    public String turnOnFan()
+    {
+        return "Fan Turned On!";
+    }
+    public String turnOffFan()
+    {
+        return "Fan Turned Off!";
+    }
+    public String IncSpeedFan()
+    {
+        return "Fan Speed Increased!";
+    }
+    public String DecSpeedFan()
+    {
+        return "Fan Speed Decreased!";
+    }
+}
